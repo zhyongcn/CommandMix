86a8c2e46d6c52f0f680444217659d8b438ce58c
zz <zhyongcn@hotmail.com>
2023-02-04 17:52:07 +0800

    init

---
+++ src/main/java/com/jiuhua/CommandMix/CommandMixApplication.java
@@ -0,0 +1,13 @@
+package com.jiuhua.CommandMix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandMixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandMixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jiuhua/CommandMix/CommandMixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiuhua.CommandMix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandMixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
